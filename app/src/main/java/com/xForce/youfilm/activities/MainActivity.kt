1733f358a241ee06ed63006ae0b2074240d33c2a
karichalobato <00002517@uca.edu.sv>
2019-06-07 07:51:04 -0600

    Innitial Commit

---
+++ app/src/main/java/com/xForce/youfilm/activities/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.xForce.youfilm.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.xForce.youfilm.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ app/src/main/java/com/xForce/youfilm/repository/movieRepository.kt
@@ -0,0 +1,4 @@
+package com.xForce.youfilm.repository
+
+class movieRepository {
+}
