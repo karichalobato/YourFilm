90addb6fb84b27f01bf943b5b74033a37661cbb7
DiegoVeega <00148816@uca.edu.sv>
2019-06-09 13:23:01 -0600

    Repository

--- app/src/main/java/com/xForce/youfilm/repository/movieRepository.kt
+++ app/src/main/java/com/xForce/youfilm/repository/movieRepository.kt
@@ -1,13 +1,28 @@
 package com.xForce.youfilm.repository
 
 import androidx.annotation.WorkerThread
+import androidx.lifecycle.LiveData
+import androidx.room.Insert
 import com.xForce.youfilm.database.daos.MovieDAO
+import com.xForce.youfilm.database.entities.Movie
 
 
 class movieRepository (private val movieDao: MovieDAO ) {
 
+    val AllMoview: LiveData<List<Movie>> = movieDao.getAllMovies()
+
     @WorkerThread
-    suspend fun delete(){
-        return movieDao.deleteMovies()
+    suspend fun getMovieByTitle(title:String){
+        movieDao.getMovieByTitle(title)
+    }
+
+    @WorkerThread
+    suspend fun deleteMovies(){
+        movieDao.deleteMovies()
+    }
+
+    @WorkerThread
+    suspend fun insertMovie(movie: Movie){
+        movieDao.deleteMovies()
     }
 }
