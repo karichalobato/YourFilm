14f49180e64f782918064dbe95967d010ee01d07
karichalobato <00002517@uca.edu.sv>
2019-06-09 11:53:53 -0600

    Creando entidad Movie y su respectivo DAO

--- app/src/main/java/com/xForce/youfilm/database/daos/MovieDAO.kt
+++ app/src/main/java/com/xForce/youfilm/database/daos/MovieDAO.kt
@@ -1,4 +1,25 @@
 package com.xForce.youfilm.database.daos
 
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.xForce.youfilm.database.entities.Movie
+
+@Dao
 interface MovieDAO {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertMovie(movie: Movie)
+
+    //TODO BUSQUEDA POR TITULO DE PELICULA
+    @Query("SELECT * FROM Movie WHERE title==:title")
+    fun getMovieByTitle(title:String): LiveData<List<Movie>>
+
+    @Query("SELECT * FROM Movie")
+    fun getAllMovies():LiveData<List<Movie>>
+
+    @Query("DELETE FROM Movie")
+    fun deleteMovies()
 }
--- app/src/main/java/com/xForce/youfilm/database/entities/Movie.kt
+++ app/src/main/java/com/xForce/youfilm/database/entities/Movie.kt
@@ -1,6 +1,27 @@
 package com.xForce.youfilm.database.entities
 
+import androidx.room.*
 
+@Entity(indices = [Index("Title")],
+    tableName = "Movie")
 
-class Movie {
+data class Movie (
+
+    @ColumnInfo(name = "Title")
+    val title: String,
+    @ColumnInfo(name = "Realeased")
+    val realeased: String,
+    @ColumnInfo(name = "Runtime")
+    val runtime: String,
+    @ColumnInfo(name = "Genre")
+    val genre: String,
+    @ColumnInfo(name = "Director")
+    val director: String,
+    @ColumnInfo(name = "Actors")
+    val actors: String
+)
+
+{
+    @PrimaryKey(autoGenerate = true)
+    var idMovie: Int = 0
 }
