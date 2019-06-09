516fcd688209537638f8ef55ea0c58cd57d1e3b6
karichalobato <00002517@uca.edu.sv>
2019-06-09 12:32:23 -0600

    movieRoomDataBase ya está con "internet" falta sin internet

--- app/src/main/java/com/xForce/youfilm/database/movieRoomDataBase.kt
+++ app/src/main/java/com/xForce/youfilm/database/movieRoomDataBase.kt
@@ -1,8 +1,35 @@
 package com.xForce.youfilm.database
 
+import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.xForce.youfilm.database.daos.MovieDAO
 import com.xForce.youfilm.database.entities.Movie
 
 @Database(entities = [Movie::class], version = 1, exportSchema = false)
-public abstract class movieRoomDataBase {
+public abstract class movieRoomDataBase : RoomDatabase(){
+
+    abstract fun movieDao(): MovieDAO
+
+    companion object {
+        @Volatile
+        private  var INSTANCE: movieRoomDataBase? = null
+
+        fun getInstance(
+            context: Context
+        ): movieRoomDataBase {
+            val tempInstance = INSTANCE
+            if (tempInstance != null){
+                return tempInstance
+            }
+            synchronized(this){
+                val instance = Room
+                    .databaseBuilder(context, movieRoomDataBase::class.java, "Movie_Database")
+                    .build()
+                INSTANCE=instance
+                return instance
+            }
+        }
+    }
 }
