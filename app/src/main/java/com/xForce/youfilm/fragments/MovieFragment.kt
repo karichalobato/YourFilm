da2c8a2f4c12c00cead807b5f53d3612476e78e5
Donali <00084417@uca.edu.sv>
2019-06-12 17:52:43 -0600

    Filling second fragment, added more fields to movie entity

--- app/src/main/java/com/xForce/youfilm/fragments/MovieFragment.kt
+++ app/src/main/java/com/xForce/youfilm/fragments/MovieFragment.kt
@@ -6,12 +6,16 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import androidx.navigation.fragment.navArgs
+import com.squareup.picasso.Picasso
 import com.xForce.youfilm.R
 import com.xForce.youfilm.viewModel.MovieViewModel
+import kotlinx.android.synthetic.main.cardview_movie.*
+import kotlinx.android.synthetic.main.fragment_movie.*
 
 
 /*// TODO: Rename parameter arguments, choose names that match
@@ -31,6 +35,7 @@ class MovieFragment : Fragment() {
 
     lateinit var movieTitle:TextView
     lateinit var movieViewModel: MovieViewModel
+    lateinit var movieImageView:ImageView
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -42,10 +47,21 @@ class MovieFragment : Fragment() {
         movieViewModel  = ViewModelProviders.of(this).get(MovieViewModel::class.java)
 
         movieTitle = view.findViewById(R.id.movie_title_main_content_fragment)
+        movieImageView = view.findViewById(R.id.image_main_content_fragment)
 
 
         movieViewModel.getMovieById(args.imID).observe(this, Observer {
             movieTitle.text = it.Title
+            Picasso.get()
+                .load(it.poster)
+                .into(movieImageView)
+            plot_main_content_fragment.text = it.Plot
+//            movie_year.text  = it.Year
+            released_main_content_fragment.text = it.Year
+            runtime_main_content_fragment.text  = it.Runtime
+            genre_main_content_fragment.text = it.Genre
+            director_main_content_fragment.text = it.Director
+            actors_main_content_fragment.text = it.actors
         })
 
         return view
