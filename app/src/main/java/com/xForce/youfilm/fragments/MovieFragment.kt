963f4eab00e4218847b0e58bf4b38b17bff54dc3
Donali <00084417@uca.edu.sv>
2019-06-12 13:17:02 -0600

    UI Interaction

--- app/src/main/java/com/xForce/youfilm/fragments/MovieFragment.kt
+++ app/src/main/java/com/xForce/youfilm/fragments/MovieFragment.kt
@@ -6,26 +6,51 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
+import androidx.navigation.fragment.navArgs
 import com.xForce.youfilm.R
+import com.xForce.youfilm.viewModel.MovieViewModel
 
 
-// TODO: Rename parameter arguments, choose names that match
+/*// TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 
-/**
+*//**
  * A simple [Fragment] subclass.
- *
- */
+ **/
+
+
+
 class MovieFragment : Fragment() {
+
+    val args:MovieFragmentArgs by navArgs()
+
+    lateinit var movieTitle:TextView
+    lateinit var movieViewModel: MovieViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_movie, container, false)
+        val view =  inflater.inflate(R.layout.fragment_movie, container, false)
+
+        movieViewModel  = ViewModelProviders.of(this).get(MovieViewModel::class.java)
+
+        movieTitle = view.findViewById(R.id.movie_title_main_content_fragment)
+
+
+        movieViewModel.getMovieById(args.imID).observe(this, Observer {
+            movieTitle.text = it.Title
+        })
+
+        return view
+
+
     }
 
 
