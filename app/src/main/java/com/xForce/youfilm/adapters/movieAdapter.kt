04f4970cfd0f0ea1e26d85b8cf05207b2d3f97e4
DiegoVeega <00148816@uca.edu.sv>
2019-06-11 21:07:59 -0600

    Mod

--- app/src/main/java/com/xForce/youfilm/adapters/movieAdapter.kt
+++ app/src/main/java/com/xForce/youfilm/adapters/movieAdapter.kt
@@ -1,4 +1,5 @@
 package com.xForce.youfilm.adapters
 
 class movieAdapter {
+
 }
--- app/src/main/java/com/xForce/youfilm/fragments/MainListFragment.kt
+++ app/src/main/java/com/xForce/youfilm/fragments/MainListFragment.kt
@@ -1,4 +1,32 @@
 package com.xForce.youfilm.fragments
 
-class MainListFragment {
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.xForce.youfilm.R
+import com.xForce.youfilm.database.entities.Movie
+
+class MainListFragment : Fragment() {
+
+    private lateinit var  movies :ArrayList<Movie>
+    var listenerTool : SearchNewMovieListener? = null
+
+    companion object {
+        fun newInstance(dataset : ArrayList<Movie>): MainListFragment{
+            val newFragment = MainListFragment()
+            newFragment.movies= dataset
+            return newFragment
+        }
+    }
+
+    interface SearchNewMovieListener{
+        fun searchMovie(movieName: String)
+
+        fun managePortraitItemClick(movie: Movie)
+
+        fun manageLandscapeItemClick(movie: Movie)
+    }
+
 }
